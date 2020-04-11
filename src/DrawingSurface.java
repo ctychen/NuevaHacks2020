c1484907512786a3ff7cafcad4896feed7af8da3
ctychen <34198336+ctychen@users.noreply.github.com>
2020-04-10 22:50:44 -0700

    yummy setup

---
+++ src/DrawingSurface.java
@@ -0,0 +1,49 @@
+import processing.event.KeyEvent;
+import processing.core.PApplet;
+
+public class DrawingSurface extends PApplet {
+
+
+	public DrawingSurface() {
+
+	}
+
+	// The statements in the setup() function
+	// execute once when the program begins
+	public void setup() {
+	}
+
+	// The statements in draw() are executed until the
+	// program is stopped. Each statement is executed in
+	// sequence and after the last line is read, the first
+	// line is executed again.
+	public void draw() {
+
+	}
+
+	public void mousePressed() {
+
+	}
+
+	public void mouseDragged() {
+
+	}
+
+	public void keyPressed() {
+		if(keyCode == 38) { // up arrow
+			
+		} else if(keyCode == 37) { // left arrow
+			
+		} else if(keyCode == 40) { // down arrow
+			
+		} else if(keyCode == 39) { // right arrow
+			
+		}
+
+	}
+
+	public void keyTyped(KeyEvent k) {
+
+	}
+
+}
