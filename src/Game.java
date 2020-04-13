11311a13ff02a08246a54df523efe31e873bdb58
osman <osman@calvins-mbp>
2020-04-13 13:12:44 -0700

    Added a couple of fields and some headers

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
 
 public class Game {
-
+	private Player player;
+	private ArrayList<Person> plist;
+	private ArrayList<Vehicle> vlist;
+	
+	public Game() {
+		
+	}
+	
+	public void updateRisk() {
+		
+	}
+	
 	
 }
