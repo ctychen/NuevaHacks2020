910cea4c816827a41604d7b1370954fed9c490b6
osman <osman@calvins-mbp>
2020-04-14 20:26:20 -0700

    Changed fields to protected, added updateGame method header+notes

--- src/Game.java
+++ src/Game.java
@@ -4,12 +4,20 @@ public class Game {
 	
 	public static final int front = 0, frontstep1 = 1, frontstep2 = 2, right = 3, rightstep1 = 4, rightstep2 = 5, left = 6, leftstep1 = 7, leftstep2 = 8, back = 9, backstep1 = 10, backstep2=11;
 	
-	private Player player;
-	private ArrayList<Person> plist;
-	private ArrayList<Vehicle> vlist;
+	protected Player player;
+	protected ArrayList<Person> plist;
+	protected ArrayList<Vehicle> vlist;
 	
 	public Game() {
 		
+	}
+	
+	public void updateGame() {
+		//calls methods to:
+		//move NPCs, cars, and pets
+		//recalculate risk and update player/npc infection status if necessary
+		//update score
+		//check game state
 	}
 	
 	public void updateRisk() {
