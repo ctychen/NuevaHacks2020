66086abc50ca0b829af92627d75eb1c74e09e1c2
osman <osman@calvins-mbp>
2020-04-15 12:36:34 -0700

    Added methods for changing risk, needs testing/adjustment

--- src/Game.java
+++ src/Game.java
@@ -7,6 +7,7 @@ public class Game {
 	protected Player player;
 	protected ArrayList<Person> plist;
 	protected ArrayList<Vehicle> vlist;
+	protected int numInfected;
 	
 	public Game() {
 		
@@ -20,10 +21,65 @@ public class Game {
 		//check game state
 	}
 	
+	//updates risk based on movement and does a check for infection
 	public void updateRisk() {
-		
+		ArrayList<Integer> risklist = riskCalc();
+		player.risk=player.initRisk+risklist.get(0);
+		if(player.risk>100) {
+			player.risk=100;
+		}
+		//update risk bar here
+		for(int i=1;i<risklist.size();i++)
+		{
+			if(!player.isInfected)
+			{
+				if(plist.get(risklist.get(i)).isInfected)
+				{
+					int x=(int)(Math.random()*100);
+					if(x<player.risk) {
+						player.isInfected=true;
+					}
+				}
+			}
+			else
+			{
+				int x=(int)(Math.random()*100);
+				if(x<player.risk) {
+					numInfected++;
+				}
+			}
+		}
 	}
 	
+	//changes initRisk by a certain amount, for risk based on reflex checks and PPE
+	public void changeInitRisk(int change) {
+		player.initRisk+=change;
+		if(player.initRisk>100) {
+			player.initRisk=100;
+		}
+		else if(player.initRisk<0) {
+			player.initRisk=0;
+		}
+	}
+	
+	//calculates distance from player to other NPCs, calculates risk based off of that
+	//returns arraylist of ints with added risk first, then the indices of all the people nearby that are infected
+	private ArrayList<Integer> riskCalc() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int addedrisk=0;
+		list.add(0);
+		for(int i=0;i<plist.size();i++)
+		{
+			Person p = plist.get(i);
+			double distance=(Math.sqrt(Math.pow(player.x-p.x, 2) + Math.pow(player.y-p.y, 2)));
+			if(distance<DrawingSurface.DEFAULT_WIDTH/20) {
+				addedrisk+=(DrawingSurface.DEFAULT_WIDTH/20-distance)*(DrawingSurface.DEFAULT_WIDTH/20-distance)/80;
+				list.add(i);
+			}
+		}
+		list.set(0, addedrisk);
+		return list;
+	}
 	
 	
 	
