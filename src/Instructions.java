c1db8ea0c3ed606711cce85be11325667c9bc9b8
ctychen <34198336+ctychen@users.noreply.github.com>
2020-04-16 13:41:25 -0700

    lmao

--- src/Instructions.java
+++ src/Instructions.java
@@ -1,15 +1,23 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Instructions extends PApplet{
 
 	protected final int DRAWING_WIDTH = 800, DRAWING_HEIGHT = 400;
 
 	private float shiftSpeed;
+	
+	private PImage how;
 
 	private SpookyColors fancyRainbow = new SpookyColors(0, 0, 0, 0, SpookyColors.HORIZONTAL);
 	
 	public Instructions() {
 		shiftSpeed = 0;
+	}
+	
+	public void setup() {
+		how = loadImage("intro.png");
+		how.resize(width/2, 2*height/3);
 	}
 	
 	public void draw() {
@@ -18,6 +26,7 @@ public class Instructions extends PApplet{
 		fancyRainbow.draw(this);
 		popStyle();
 		fill(0);
+		image(how, width/4, (int)(Math.random()*2*height/7));
 		textAlign(CENTER);
 		textSize(30);
 		textSize(width*(float)Math.random()*0.02f+0.03f*width);
@@ -25,7 +34,6 @@ public class Instructions extends PApplet{
 		translate(width/2 - 200, height/2 - 190);
 		fill((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
 		text("Game Instructions", width/4+(int)(Math.random()*10), (int)(Math.random()*height/7));
-		
 	}
 	
 }
