752707f29e4a70a67fcd8e6429834e407ba0f0bd
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2020-04-16 23:59:00 -0700

    improved the reactions

--- src/ReactionKeySave.java
+++ src/ReactionKeySave.java
@@ -5,6 +5,8 @@ public class ReactionKeySave {
 	int[] keyCodes = {67, 69, 70, 71, 72, 73, 76, 80, 81, 82, 84, 75, 77};
 	char[] buttons = {'C', 'E', 'F', 'G', 'H', 'I', 'L', 'P', 'Q', 'R', 'T', 'K', 'M'};
 	String[] messages = {"to not touch your face", "to dodge a sneeze", "to dodge a cough"};
+	String[] congrats = {"avoided touching your face", "dodged a sneeze", "dodged a cough"};
+	String[] reee = {"touched your face", "got hit by a sneeze", "got coughed on"};
 	int key;
 	int message;
 	int maxTime;
@@ -21,14 +23,14 @@ public class ReactionKeySave {
 	public void draw(PApplet g, boolean keys[]) {
 		g.pushStyle();
 		g.fill((float)Math.random()*255, (float)Math.random()*255, (float)Math.random()*255);
-		g.rect(g.width*0.4f, g.height*0.4f, g.width*0.2f, g.height*0.2f);
+		g.rect(g.width*0.2f, g.height*0.4f, g.width*0.4f, g.height*0.1f);
 		g.fill(0);
 		if ((keys[keyCodes[key]] || saved == 1) && saved != -1) { //saved
-			g.text("NICE SAVE", g.width*0.45f, g.height*0.45f);
+			g.text("NICE SAVE, you " + congrats[message], g.width*0.45f, g.height*0.45f);
 			saved = 1;
 			
 		} else if ((time > maxTime || saved == -1) && saved != 1) {
-			g.text("TOO SLOW!", g.width*0.5f, g.height*0.5f);
+			g.text("TOO SLOW! You " + reee[message], g.width*0.5f, g.height*0.5f);
 				
 			saved = -1;
 		
