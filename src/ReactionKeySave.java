605fdfa9dd22cf80f053e582efcf921b35597563
ctychen <34198336+ctychen@users.noreply.github.com>
2020-04-17 00:31:15 -0700

    woop

--- src/ReactionKeySave.java
+++ src/ReactionKeySave.java
@@ -4,9 +4,9 @@ public class ReactionKeySave {
 	
 	int[] keyCodes = {67, 69, 70, 71, 72, 73, 76, 80, 81, 82, 84, 75, 77};
 	char[] buttons = {'C', 'E', 'F', 'G', 'H', 'I', 'L', 'P', 'Q', 'R', 'T', 'K', 'M'};
-	String[] messages = {"to not touch your face", "to dodge a sneeze", "to dodge a cough, to not rub your eyes"};
-	String[] congrats = {"avoided touching your face", "dodged a sneeze", "dodged a cough, didn't rub your eyes"};
-	String[] reee = {"touched your face", "got hit by a sneeze", "got coughed on, rubbed your eyes"};
+	String[] messages = {"to not touch your face", "to dodge a sneeze", "to dodge a cough", "to not rub your eyes"};
+	String[] congrats = {"avoided touching your face", "dodged a sneeze", "dodged a cough", "didn't rub your eyes"};
+	String[] reee = {"touched your face", "got hit by a sneeze", "got coughed on", "rubbed your eyes"};
 	int key;
 	int message;
 	int maxTime;
@@ -30,7 +30,7 @@ public class ReactionKeySave {
 			saved = 1;
 			
 		} else if ((time > maxTime || saved == -1) && saved != 1) {
-			g.text("TOO SLOW! You " + reee[message], g.width*0.45f, g.height*0.5f);
+			g.text("TOO SLOW! You " + reee[message], g.width*0.45f, g.height*0.45f);
 				
 			saved = -1;
 		
